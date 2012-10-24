47fc7192f8e20b502e7d0a9dbc11e711fa408570
Henric Edwards <henric.edwards@telia.com>
2012-10-24 10:36:04 +0200

    Added simple tests to show boxing

---
+++ src/Examples/Examples/2_refandvalue/BoxingAndUnboxingExamples.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Examples._2_refandvalue
+{
+    [TestFixture]
+    public class BoxingAndUnboxingExamples
+    {
+        [Test]
+        public void Boxing_Creates_A_New_Object_But_Doesnt_referr_To_The_Old_Value_Type_By_Ref()
+        {
+            int a = 10;
+
+            //Boxing
+            object o = a;
+
+            Assert.AreNotSame(a, o);
+        }
+
+        [Test]
+        public void Boxing_Creates_A_New_Object_But_Doesnt_referr_To_The_Old_Value_Type_By_Ref_Part_2()
+        {
+            int a = 10;
+
+            //Boxing
+            object o = a;
+
+            a = 12;
+
+            Assert.AreEqual(12, a);
+            Assert.AreEqual(10, o);
+            Assert.AreNotSame(a, o);
+        }
+
+        [Test]
+        [ExpectedException(typeof(InvalidCastException))]
+        public void Invalid_Unboxing_Throws_Invalid_Cast_Exception()
+        {
+            long a = 12;
+
+            object o = a;
+
+            var i = (int) o;
+        }
+    }
+}
