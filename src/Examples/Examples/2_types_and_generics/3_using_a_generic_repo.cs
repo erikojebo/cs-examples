e0a8cc9b3ce84c9a2567c170531606559fd796ef
Henric Edwards <henric.edwards@telia.com>
2012-11-16 08:41:13 +0100

    Removed member repositories for even more clarity! (Shiny!) Also added case with sub-classed repo of StockItems.

--- src/Examples/Examples/2_types_and_generics/3_using_a_generic_repo.cs
+++ src/Examples/Examples/2_types_and_generics/3_using_a_generic_repo.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using NUnit.Framework;
 
 namespace Examples._2_types_and_generics
@@ -9,24 +7,15 @@ namespace Examples._2_types_and_generics
     [TestFixture]
     public class _3_using_a_generic_repo
     {
-        private UserRepository _repository;
-        private Repository<Person> _entityRepository;
-
-
-        [SetUp]
-        public void Test_Fixture_SetUp()
-        {
-            _repository = new UserRepository();
-            _entityRepository = new Repository<Person>();
-
-        }
 
         [Test]
         public void We_can_add_user_through_our_generic_repo()
         {
-            _repository.Add(new User{Id = 3});
+            var userRepo = new UserRepository();
 
-            var user = _repository.FindById(3);
+            userRepo.Add(new User { Id = 3 });
+
+            var user = userRepo.FindById(3);
 
             Assert.AreEqual(3, user.Id);
         }
@@ -34,11 +23,13 @@ namespace Examples._2_types_and_generics
         [Test]
         public void We_also_get_the_whole_list_of_typed_objects_back_from_repo()
         {
-            _repository.Add(new User{Id = 3});
-            _repository.Add(new User{Id = 4});
-            _repository.Add(new User { Id = 6 });
+            var userRepo = new UserRepository();
 
-            var actual = _repository.FindAll().Count;
+            userRepo.Add(new User { Id = 3 });
+            userRepo.Add(new User { Id = 4 });
+            userRepo.Add(new User { Id = 6 });
+
+            var actual = userRepo.FindAll().Count;
            
             Assert.AreEqual(3, actual);
         }
@@ -46,12 +37,14 @@ namespace Examples._2_types_and_generics
         [Test]
         public void We_can_put_type_constraints_on_our_generic_structures()
         {
-            _entityRepository.Add(new Person{Id = 5, Name = "Erik"});
-            _entityRepository.Add(new Person { Id = 6, Name = "DAFO"});
+            var entityRepo = new Repository<Person>();
 
-            var actual = _entityRepository.FindAll().Count;
-            var firstPerson = _entityRepository.FindById(5);
-            var secondPerson = _entityRepository.FindById(6);
+            entityRepo.Add(new Person { Id = 5, Name = "Erik" });
+            entityRepo.Add(new Person { Id = 6, Name = "DAFO" });
+
+            var actual = entityRepo.FindAll().Count;
+            var firstPerson = entityRepo.FindById(5);
+            var secondPerson = entityRepo.FindById(6);
             
 
             Assert.AreEqual(2, actual);
@@ -62,21 +55,47 @@ namespace Examples._2_types_and_generics
         [Test]
         public void But_we_can_still_act_on_derived_types_own_members()
         {
-            var expected = new Person {Id = 1, Name = "Henric"};
-            _entityRepository.Add(expected);
+            var entityRepo = new Repository<StockItem>();
 
-            var actual = _entityRepository.FindById(1);
+            var expected = new StockItem {Id = 1, ItemName = "Useless Part", Category = "Parts"};
+            entityRepo.Add(expected);
+
+            var actual = entityRepo.FindById(1);
 
             Assert.AreEqual(expected, actual);
-            Assert.AreEqual("Henric", actual.Name);
+            Assert.AreEqual("Useless Part", actual.ItemName);
+            Assert.AreEqual("Parts", actual.Category);
         }
 
-        [TearDown]
-        public void RemoveTempEntriesFromInMemDbs()
+        [Test]
+        public void Subclassing_helps_us_with_cases_specially_tailored_to_a_certain_type()
         {
-            _entityRepository = new Repository<Person>();
-            _repository = new UserRepository();
+            var entityRepo = new ItemRespository();
+
+            var item1 = new StockItem { Id = 1, ItemName = "Useless Part", Category = "Parts" };
+            var item2 = new StockItem { Id = 2, ItemName = "Useless Part 2", Category = "Parts" };
+            var item3 = new StockItem { Id = 3, ItemName = "Useless Part 2", Category = "Misc" };
+            
+            entityRepo.Add(item1);
+            entityRepo.Add(item2);
+            entityRepo.Add(item3);
+
+            var items = entityRepo.FindByCategoryName("Parts");
+            var found1 = items[0];
+            var found2 = items[1];
+
+            Assert.AreEqual(2, items.Count);
+            Assert.AreEqual(item1, found1);
+            Assert.AreEqual(item2, found2);
+
         }
+
+        //[Test]
+        //public void Type_constraint_hinders_us_from_using_other_types()
+        //{
+        //    //No go
+        //    var repo = new Repository<User>();
+        //}
     }
 
     #region Regular Interface Repo
@@ -120,7 +139,7 @@ namespace Examples._2_types_and_generics
     //This way we can get ALL our entities in a domain model through a simple base class repo of T
     public class Repository<T> where T : EntityBase
     {
-        private List<T> _repo = new List<T>();
+        protected List<T> _repo = new List<T>();
 
         public void Add(T t)
         {
@@ -149,6 +168,26 @@ namespace Examples._2_types_and_generics
     {
         public string Name { get; set; }
     }
+
+    public class StockItem : EntityBase
+    {
+        public string Category { get; set; }
+        public string ItemName { get; set; }
+    }
     #endregion
+
+#region Subclassing for specialized search
+
+    public class ItemRespository : Repository<StockItem>
+    {
+        public List<StockItem> FindByCategoryName(string category)
+        {
+            var items = _repo.Where(x => x.Category == category).ToList();
+
+            return items;
+        }
+    }
+
+#endregion
 
 }
