c008749ed5431f271aa1996bf26d559606bbc3d3
Erik Öjebo <erik.ojebo@gmail.com>
2013-01-17 12:52:26 +0100

    Initial commit of code smell mvc app

---
+++ src/Examples/_6_code_smells/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml.Linq;
+
+namespace _6_code_smells.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var path = Path.Combine(Path.GetTempPath(), "_6_code_smells.xml");
+            var xml = System.IO.File.ReadAllText(path);
+            var document = XDocument.Parse(xml);
+
+
+            var contactNodes = document.Descendants("Contact")
+                                       .Select(x =>
+                                           {
+                                               var dictionary = new Dictionary<string, string>();
+
+                                               dictionary["PhoneNumber"] = x.Attribute("PhoneNumber").Value;
+                                               dictionary["LastName"] = x.Attribute("LastName").Value;
+                                               dictionary["FirstName"] = x.Attribute("FirstName").Value;
+                                               dictionary["EmailAddress"] = x.Attribute("EmailAddress").Value;
+
+                                               return dictionary;
+                                           }).OrderBy(x => x["LastName"])
+                                           .ToList();
+
+            return View(contactNodes);
+        }
+    }
+}
---
+++ src/Examples/_6_code_smells/Global.asax.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace _6_code_smells
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            CreateDummyData();
+        }
+
+        private void CreateDummyData()
+        {
+            var fileContent = @"<Contacts>
+    <Contact FirstName='Kalle' LastName='Karlsson' PhoneNumber='555-123456' EmailAddress='kalle.karlsson@swipnet.se' />
+    <Contact FirstName='Pelle' LastName='Svensson' PhoneNumber='555-123456' EmailAddress='pelle.svensson@swipnet.se' />
+    <Contact FirstName='Nisse' LastName='Nilssson' PhoneNumber='555-123456' EmailAddress='nisse.nilssson@swipnet.se' />
+    <Contact FirstName='Sture' LastName='Karlsson' PhoneNumber='555-123456' EmailAddress='sture.karlsson@swipnet.se' />
+</Contacts>";
+
+            const string fileName = "_6_code_smells.xml";
+            var path = Path.Combine(Path.GetTempPath(), fileName);
+            File.WriteAllText(path, fileContent);
+        }
+    }
+}
